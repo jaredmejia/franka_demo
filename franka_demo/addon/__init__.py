2465e4b8e89479e939062248e4e7e8936a2194ea
Kay Ke <dustocyno@gmail.com>
2021-10-18 19:23:30 -0400

    Add a function: replay

--- franka_demo/addon/__init__.py
+++ franka_demo/addon/__init__.py
@@ -1,3 +1,4 @@
 from .teleop import add_teleop_function
 from .sinwave import add_sinwave_function
 from .logging import add_logging_function
+from .replay import add_replay_function
--- franka_demo/scripts/test_addon.py
+++ franka_demo/scripts/test_addon.py
@@ -4,11 +4,13 @@ from time import time, sleep
 
 from franka_demo.hardware_franka import get_args
 from franka_demo.demo_interfaces import run_demo
-from franka_demo.addon import add_teleop_function, add_logging_function
+from franka_demo.addon import add_teleop_function, add_logging_function, add_sinwave_function, add_replay_function
 
 def callback_func(state):
     add_teleop_function(state)
     add_logging_function(state)
+    add_sinwave_function(state)
+    add_replay_function(state)
 
 if __name__ == "__main__":
 
@@ -16,5 +18,6 @@ if __name__ == "__main__":
 
     run_demo(callback_func, params={
         'ip_address': args.server_ip,
-        'log_folder': 'logs'
+        'log_folder': 'logs',
+        'replay_filename': 'franka_demo/logs/21-10-14-19-50-07.csv'
     })
